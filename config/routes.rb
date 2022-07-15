1cb1466a199d41d0285b0e2e5ae7d36dbac6f889
tresorsawasawa <tresorsawasawa@gmail.com>
2022-07-15 11:36:20 +0200

    Add the authentication and validation on the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  }
 end
